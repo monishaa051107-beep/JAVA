e36198723037bb744e601f1880a7f96dce07dcf2
monishaa051107-beep <monishaa051107@gmail.com>
2026-05-04 22:07:32 -0700

    Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 5;
+        int a = 0, b = 1;
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
